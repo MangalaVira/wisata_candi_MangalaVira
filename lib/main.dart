9dbfdf307245d7bb3c4899ec048b1a9db22868ac
MangalaVira <mangalaviravalentino_2428250045@mhs.mdp.ac.id>
2025-11-05 11:20:15 +0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,49 @@
 import 'package:flutter/material.dart';
 import 'package:wisata_candi/data/candi_data.dart';
+import 'package:wisata_candi/models/candi.dart';
+// ignore: unused_import
 import 'package:wisata_candi/screens/detail_screen.dart';
 
 void main() {
   runApp(const MyApp());
+}
+
+class ProfileScreen extends StatefulWidget{
+  const ProfileScreen({super.key, required Candi candi});
+
+  @override
+  State<ProfileScreen> createState() => _ProfileScreenState();
+}
+
+class _ProfileScreenState extends State<ProfileScreen> {
+  // TODO: 1. Deklarasikan variabel yang dibutuhkan
+  bool isSignedIn = false;
+  String fullName = '';
+  String userName = '';
+  int favoriteCandiCount = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          Container(
+            height: 200, width: double.infinity, color: Colors.deepPurple,
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16),
+            child: Column(
+              children: [
+                // TODO: 2. Buat bagian ProfileHeader yang berisi gambar profil
+                // TODO: 3. Buat bagian ProfileInfo yang berisi info profile 
+                // TODO: 4. Buat ProfileActions yang berisi TextButton sign in/out
+             ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
 }
 
 class MyApp extends StatelessWidget {
@@ -16,7 +56,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: DetailScreen(candi: candiList[0]),
+      home: ProfileScreen(candi: candiList[0]),
     );
   }
 }
