5ced79805a92f8728dd21fbe3020ef50e404ff9e
FernandoFeliansyah <mangalaviravalentino_2428250045@mhs.mdp.ac.id>
2025-11-06 11:46:38 +0700

    profile

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
+// ignore: unused_import
 import 'package:wisata_candi/data/candi_data.dart';
-import 'package:wisata_candi/models/candi.dart';
 // ignore: unused_import
 import 'package:wisata_candi/screens/detail_screen.dart';
 
@@ -8,19 +8,32 @@ void main() {
   runApp(const MyApp());
 }
 
-class ProfileScreen extends StatefulWidget{
-  const ProfileScreen({super.key, required Candi candi});
+class ProfileScreen extends StatefulWidget {
+  const ProfileScreen({super.key});
 
   @override
   State<ProfileScreen> createState() => _ProfileScreenState();
 }
 
 class _ProfileScreenState extends State<ProfileScreen> {
-  // TODO: 1. Deklarasikan variabel yang dibutuhkan
+  // TODO: 1 Deklarasikan variabel yang dibutuhkan
   bool isSignedIn = false;
   String fullName = '';
-  String userName = '';
+  String username = '';
   int favoriteCandiCount = 0;
+
+  // TODO: 5. Implementasi fungsi Sign In
+  void signIn() {
+    setState(() {
+      isSignedIn = !isSignedIn;
+    });
+  }
+  // TODO: 6. IMplementasi fungsi Sign Out
+  void signOut() {
+    setState(() {
+      isSignedIn = !isSignedIn;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -34,10 +47,76 @@ class _ProfileScreenState extends State<ProfileScreen> {
             padding: const EdgeInsets.symmetric(horizontal: 16),
             child: Column(
               children: [
-                // TODO: 2. Buat bagian ProfileHeader yang berisi gambar profil
-                // TODO: 3. Buat bagian ProfileInfo yang berisi info profile 
-                // TODO: 4. Buat ProfileActions yang berisi TextButton sign in/out
-             ],
+                // TODO: 2 Buat bagian ProfileHeader yang berisi gambar profil
+                Align(
+                  alignment:AlignmentGeometry.topCenter,
+                  child: Padding(
+                    padding: const EdgeInsets.only(top: 200-50),
+                    child: Stack(
+                      alignment: Alignment.bottomRight,
+                      children: [
+                        Container(
+                          decoration: BoxDecoration(
+                            border: Border.all(color: Colors.deepPurple, width: 2),
+                            shape: BoxShape.circle,
+                          ),
+                          child: CircleAvatar(
+                            radius: 50,
+                            backgroundImage: AssetImage('images/placeholder_image.png'),
+                          ),
+                        ),
+                        if(isSignedIn)
+                          IconButton(
+                            onPressed: (){}, 
+                            icon: Icon(Icons.camera_alt, color: Colors.deepPurple[50],),),
+                      ],
+                    ),
+                  ),
+                ),
+                // TODO: 3 Buat bagian ProfileInfo yang berisi info profil
+                SizedBox(height: 20),
+                Divider(color: Colors.deepPurple[100]),
+                SizedBox(height: 4),
+                ProfileInfoItem(
+                  icon: Icons.lock, 
+                  label: 'Pengguna', 
+                  value: username, 
+                  iconColor: Colors.amber,
+                ),
+                SizedBox(height: 4),
+                Divider(color: Colors.deepPurple[100]),
+                SizedBox(height: 4),
+                ProfileInfoItem(
+                  icon: Icons.person, 
+                  label: 'Nama', 
+                  value: fullName,
+                  showEditIcon: isSignedIn,
+                  onEditPressed: () {
+                    // Tindakan saat ikon edit ditekan, misalnya: pengeditan nama
+                    debugPrint('Icon edit ditekan ...');
+                    }, 
+                  iconColor: Colors.blue
+                ),
+                SizedBox(height: 4),
+                Divider(color: Colors.deepPurple[100]),
+                SizedBox(height: 4),
+                ProfileInfoItem(
+                  icon: Icons.favorite, 
+                  label: 'Favorit', 
+                  value: favoriteCandiCount > 0 ? '$favoriteCandiCount' : '', 
+                  iconColor: Colors.red
+                ),
+                // TODO: 4 Buat ProfileActions yang berisi TextButton sign in/sign out
+                SizedBox(height: 4),
+                Divider(color: Colors.deepPurple[100]),
+                SizedBox(height: 20),
+                isSignedIn ? TextButton(
+                  onPressed: signOut,
+                  child: Text('Sign Out'))
+                  : TextButton(
+                    onPressed: (){}, 
+                    child: Text('Sign In')),
+              ],
             ),
           ),
         ],
@@ -55,8 +134,52 @@ class MyApp extends StatelessWidget {
       title: 'Wisata Candi',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true
       ),
-      home: ProfileScreen(candi: candiList[0]),
+      home: ProfileScreen(),
+    );
+  }
+}
+
+class ProfileInfoItem extends StatelessWidget {
+  final IconData icon;
+  final String label;
+  final String value;
+  final bool showEditIcon;
+  final VoidCallback? onEditPressed;
+  final Color iconColor;
+
+  const ProfileInfoItem({super.key,
+    required this.icon,
+    required this.label,
+    required this.value,
+    this.showEditIcon = false,
+    this.onEditPressed,
+    required this.iconColor,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        SizedBox(
+          width: MediaQuery.of(context).size.width / 3,
+          child: Row(
+            children: [
+              Icon(icon, color: iconColor), // Gunakan variabel warna ikon 
+              SizedBox(width: 8),
+              Text(
+                label,
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+              ),
+            ],
+          ),
+        ),
+        Expanded(
+          child: Text(': $value', style: TextStyle(fontSize: 18)),
+          ),
+        if(showEditIcon) InkWell(onTap: onEditPressed, child: Icon(Icons.edit)),
+      ],
     );
   }
 }
